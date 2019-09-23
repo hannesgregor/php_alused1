b147a7169b98020c9cd99df6d76f661a9eb32804
hannesgregor <hannes-gregor.galkin@khk.ee>
2019-09-23 11:38:56 +0300

    PHP - Printimine ja echo abil väljastamine

---
+++ 23.09.2019/print.php
@@ -0,0 +1,16 @@
+<?php
+
+//Üherealine kommentaar - printimine funktsiooni abil
+
+print "See on esimene php rida";
+print "See on teine php rida";
+print "See on kolmas php rida<br>";
+print "proov<br>";
+/*
+ * See on
+ * Mitmerealine
+ * kommentaar
+ */
+
+// echo abil väljastamine
+echo 'See on echo tekst';
