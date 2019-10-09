7991ee97c39ba66d50c0d5028401a522174ca2a5
hannesgregor <hannes-gregor.galkin@khk.ee>
2019-10-09 14:47:36 +0300

    tunnitöö

---
+++ 09.10.2019/aritmeetika.php
@@ -0,0 +1,38 @@
+<?php
+
+
+$x = 8;
+$y = 2;
+
+// additional variables
+
+$sum = $x + $y;
+$difference = $x - $y;
+$product = $x * $y;
+$quotient = $x / $y;
+$residual = $x % $y:
+
+//output table
+echo
+    '
+<table>
+        <thead>
+            <tr>
+                <th>Operaator</th>
+                <th>Nimetus</th>
+                <th>Näide</th>
+                <th>Tulemus</th>
+            </tr>
+        </thead>
+        <tbody>
+        
+            <tr>
+                <td> x+y </td>
+                <td> Liitmine </td>
+                <td> $x + $y</td>
+                <td></td>
+        </tr>
+        </tbody>
+    </table>
+'
+
